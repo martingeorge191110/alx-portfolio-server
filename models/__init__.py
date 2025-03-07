807238405e9666614cf88898b16fcafc6d0c5860
M-sharkawy <mostafasharks021@gmail.com>
2025-03-08 01:59:00 +0200

    add models and relations

--- models/__init__.py
+++ models/__init__.py
@@ -6,3 +6,9 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 from models.user import User
+from models.company import Company
+from models.notification import Notification
+from models.investment_deal import InvestmentDeal
+from models.company_owners import CompanyOwner
+from models.company_growth_rate import CompanyGrowthRate
+from models.company_docs import CompanyDocs
